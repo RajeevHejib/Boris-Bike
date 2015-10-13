63e9f18954a3c352ff57f7785077a6edec4c6433
Rajeev Hejib <rajeev_hejib@yahoo.com>
2015-10-13 14:27:04 +0100

    challenge 13

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,15 +1,28 @@
 class DockingStation
 
+  attr_reader :bike, :capacity
+
   def release_bike
-    Bike.new
+    fail "No bikes available" unless @bike
+    @bike
+  end
+
+  def dock_bike(bike)
+    fail "Docking Station full" if @bike
+    @bike = bike
+  end
+
+  def bike
+    @bike
   end
 
 end
 
 class Bike
 
+
   def working?
-    true
+    @bike
   end
 
 end
--- spec/borisbike_test_spec.rb
+++ spec/borisbike_test_spec.rb
@@ -4,7 +4,21 @@ describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
 
-  it { expect(Bike.new.working?).to eq true }
+  it { expect(Bike.new.working?).to eq @bike }
+
+  it { is_expected.to respond_to(:dock_bike).with(1).argument }
+
+  it "returns docked bikes" do
+    bike = Bike.new
+    expect(subject.dock_bike(bike)).to eq bike
+  end
+
+  it { expect{subject.release_bike}.to raise_error "No bikes available" }
+
+  it "returns docking station full" do
+    subject.dock_bike(Bike.new)
+    expect{subject.dock_bike Bike.new }.to raise_error "Docking Station full"
+  end
 
 
 end
