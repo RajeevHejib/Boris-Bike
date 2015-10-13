54fa6d0162bdc1fe74cc5c05efdd7e7aba5d2fd1
Rajeev Hejib <rajeev_hejib@yahoo.com>
2015-10-13 15:56:01 +0100

    challenge 14

--- spec/borisbike_test_spec.rb
+++ spec/borisbike_test_spec.rb
@@ -8,17 +8,15 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:dock_bike).with(1).argument }
 
-  it "returns docked bikes" do
-    bike = Bike.new
-    expect(subject.dock_bike(bike)).to eq bike
+  it "return no bikes available" do
+    expect { subject.release_bike }.to raise_error "No bikes available"
   end
 
-  it { expect{subject.release_bike}.to raise_error "No bikes available" }
-
-  it "returns docking station full" do
-    subject.dock_bike(Bike.new)
-    expect{subject.dock_bike Bike.new }.to raise_error "Docking Station full"
+  it "return bike to docking station" do
+    20.times { subject.dock_bike(Bike.new) }
+    expect { subject.dock_bike Bike.new }.to raise_error "Docking Station full"
   end
+
 
 
 end
