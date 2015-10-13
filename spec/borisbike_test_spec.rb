20f122e004ea4de95052639ce11ed8ce7fdf9abc
Rajeev Hejib <rajeev_hejib@yahoo.com>
2015-10-13 11:56:18 +0100

    challenge 10

--- spec/borisbike_test_spec.rb
+++ spec/borisbike_test_spec.rb
@@ -1,3 +1,10 @@
-describe dockingStation do
-  
+require 'DockingStation.rb'
+
+describe DockingStation do
+
+  it { is_expected.to respond_to :release_bike }
+
+  it { expect(Bike.new.working?).to eq true }
+
+
 end
